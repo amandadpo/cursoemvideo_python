e7660c279fb322f16c3ee16aa4f6acf20724c125
Amanda de Oliveira Pereira <111541175+amandadpo@users.noreply.github.com>
2023-01-08 16:18:48 -0300

    ex 23

---
+++ ex023.py
@@ -0,0 +1,17 @@
+num = input('Digite um número até 9999: ')
+try:
+    num = int(num)
+    if num > 0 and num < 9999:
+        u = num  //  1 % 10
+        d = num // 10 % 10
+        c = num // 100 % 10
+        m = num // 1000 % 10
+        print('Analisando o número {}'.format(num))
+        print('Unidade: {}'.format(u))
+        print('Dezena: {}'.format(d))
+        print('Centena: {}'.format(c))
+        print('Milhar: {}'.format(m))
+    else:
+         print('Digite um número válido')
+except ValueError:
+    print('Digite somente números inteiros')
