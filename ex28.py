3b0958ebfd348cc1df347b263bf1007f5c24d916
Amanda de Oliveira Pereira <111541175+amandadpo@users.noreply.github.com>
2023-01-13 12:12:07 -0300

    ex 28

---
+++ ex28.py
@@ -0,0 +1,22 @@
+from random import randint
+from time import sleep
+
+print('Vamos jogar! Tente advinhar o número que eu pensar!')
+print("Computador pensando...")
+sleep(1)
+print('*' * 30)
+
+computador = randint(0,5)
+jogador = int(input('Digite um número de 0 a 5: '))
+
+
+if jogador >= 0 and jogador <= 5:
+    if computador == jogador:
+        print('Parabéns! Você acertou!')
+        print(f'Você digitou nº {jogador} e eu também.')
+    else:
+        print('Você errou!')
+        print(f'Você digitou o nº {jogador} e eu pensei no nº {computador}')
+else:
+    print('Digite somente números de 0 a 5')
+
