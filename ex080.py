9c95e99a20d03c362196b753c438c6a164d7f138
Amanda de Oliveira Pereira <111541175+amandadpo@users.noreply.github.com>
2023-02-13 11:12:32 -0300

    ex 080

---
+++ ex080.py
@@ -0,0 +1,19 @@
+numeros = []
+for c in range(0, 5):
+    n = int(input('Digite um valor: '))
+    if c == 0 or n > numeros[-1]:
+        numeros.append(n)
+        print('Adicionado ao final da lista')
+    else:
+        posicao = 0
+        while posicao < len(numeros):
+            if n <= numeros[posicao]:
+                numeros.insert(posicao,n)
+                print(f'Adicionado na posição {posicao} da lista')
+                break
+            posicao += 1 
+print(f'Os valores digitados em ordem foram {numeros}')
+
+
+
+
