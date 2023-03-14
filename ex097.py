d11f1f9974dd9fe94361223680e615c1eea41d06
Amanda de Oliveira Pereira <111541175+amandadpo@users.noreply.github.com>
2023-03-14 14:45:59 -0300

    ex 097

---
+++ ex097.py
@@ -0,0 +1,11 @@
+def escreva(txt):
+    tamanho = len(txt) + 4
+    print('-' * tamanho)
+    print(f'  {txt}')
+    print('-' * tamanho)
+
+
+escreva('Olá mundo')
+escreva('Amanda')
+escreva('Oi')
+escreva('Paralelepipedo')
