86dfed4823eca6daa329d6364113ad23b2d5f358
Amanda de Oliveira Pereira <111541175+amandadpo@users.noreply.github.com>
2023-03-24 14:17:26 -0300

    ex 108

---
+++ ex108/moeda.py
@@ -0,0 +1,18 @@
+def metade(n):
+    m = n/2
+    return m
+
+def dobro(n):
+    d = n * 2
+    return d
+
+def aumentar(n):
+    a = (n * 0.1) + n
+    return a
+
+def diminuir(n):
+    r = n - (n * 0.13)
+    return r
+
+def moeda(preço = 0, moeda = "R$"):
+    return f'{moeda} {preço:>.2f}'.replace('.',',')
