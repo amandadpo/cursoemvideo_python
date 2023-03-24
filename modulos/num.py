6a716a047bdd2013ab7d46ccb7d8c1172c29b974
Amanda de Oliveira Pereira <111541175+amandadpo@users.noreply.github.com>
2023-03-24 12:18:15 -0300

    módulos

---
+++ modulos/num.py
@@ -0,0 +1,19 @@
+import uteis
+
+num = int(input('Digite um valor: '))
+fat = uteis.fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'O dobro de {num} é {uteis.dobro(num)}')
+print(f'O triplo de {num} é {uteis.triplo(num)}')
+
+
+'''
+Vantagens da modularização: 
+
+- Organização do código
+- Facilitação da manutenção
+- Ocultação de código detalhado
+- Reutilização em outros projetos
+
+'''
+
